c4fe7e68090d5a5dcf8c6bd21430c45f0ee1c323
brj1 <brj1@sfu.ca>
2018-01-31 15:22:25 -0800

    fixed BEAUTI interface a bit

--- src/beast/app/beauti/BBDPriorInputEditor.java
+++ src/beast/app/beauti/BBDPriorInputEditor.java
@@ -1,16 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* File BBDPrior.java
+*
+* Copyright (C) 2017 Bradley R. Jones bjones@cfenet.ubc.ca
+*
+* This file is part of BBD.
+* See the NOTICE file distributed with this work for additional
+* information regarding copyright ownership and licensing.
+*
+* BBD is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+*  BBD is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with BBD; if not, write to the
+* Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+* Boston, MA  02110-1301  USA
+*/
 package beast.app.beauti;
 
 import beast.app.draw.BEASTObjectPanel;
-import beast.app.draw.BEASTObjectInputEditor;
-import beast.core.Distribution;
-import beast.core.Logger;
-import beast.core.State;
-import beast.core.StateNode;
+import beast.app.draw.InputEditor;
 import beast.evolution.alignment.TaxonSet;
 import beast.evolution.tree.Tree;
 import beast.math.distributions.BBDPrior;
@@ -25,51 +40,198 @@ import java.awt.event.ActionListener;
 import beast.core.util.Log;
 import beast.core.BEASTInterface;
 import beast.core.Input;
+import beast.core.Operator;
 import beast.evolution.alignment.Taxon;
-import beast.math.distributions.ParametricDistribution;
+import beast.evolution.operators.TipDatesRandomWalker;
 import java.util.HashSet;
 import java.util.Set;
+import javax.swing.Box;
+import javax.swing.JComboBox;
 
 /**
- *
- * @author bjones
+ * Input editor for blind dating prior.
+ * @author Bradley R. Jones
  */
-public class BBDPriorInputEditor extends BEASTObjectInputEditor implements PriorProvider {
-    @Override
-    public Class<?> type() {
-        return BBDPriorInputEditor.class;
-    }
-	
-
-    public BBDPriorInputEditor() {
-        super(null);
-    }
-    
+public class BBDPriorInputEditor extends InputEditor.Base {
     public BBDPriorInputEditor(BeautiDoc doc) {
         super(doc);
     }
     
     @Override
-    public void init(Input<?> input, BEASTInterface beastObject, int itemNr, ExpandOption isExpandOption,
+    public Class<?> type() {
+        return BBDPrior.class;
+    }
+    
+    @Override
+    public void init(Input<?> input, BEASTInterface beastObject, final int listItemNr, ExpandOption isExpandOption,
 			boolean addButtons) {
-        super.init(input, beastObject, itemNr, isExpandOption, addButtons);
-        
-        System.out.println("please run this");
-        
+//        super.init(input, beastObject, itemNr, isExpandOption, addButtons);
+        m_bAddButtons = addButtons;
+        m_input = input;
+        m_beastObject = beastObject;
+        this.itemNr= listItemNr;
+		
+        Box itemBox = Box.createHorizontalBox();
+
+        BBDPrior prior = (BBDPrior) beastObject;
+        String text = prior.getID();
+
+        JButton taxonButton = new JButton(text);
+//        taxonButton.setMinimumSize(Base.PREFERRED_SIZE);
+//        taxonButton.setPreferredSize(Base.PREFERRED_SIZE);
+        itemBox.add(taxonButton);
+        taxonButton.addActionListener(e -> {
+                List<?> list = (List<?>) m_input.get();
+                BBDPrior prior2 = (BBDPrior) list.get(itemNr);
+                try {
+                    TaxonSet taxonset = prior2.taxonsetInput.get();
+                    List<Taxon> originalTaxa = new ArrayList<>();
+                    originalTaxa.addAll(taxonset.taxonsetInput.get());
+                    Set<Taxon> candidates = getTaxonCandidates(prior2);
+                    TaxonSetDialog dlg = new TaxonSetDialog(taxonset, candidates, doc);
+                    if (dlg.showDialog()) {
+        	            if (dlg.taxonSet.taxonsetInput.get().isEmpty()) {
+        	            	JOptionPane.showMessageDialog(doc.beauti, "At least one taxon should be included in the taxon set",
+        	            			"Error specifying taxon set", JOptionPane.ERROR_MESSAGE);
+        	            	taxonset.taxonsetInput.get().addAll(originalTaxa);
+        	            	return;
+        	            }
+
+                        prior2.taxonsetInput.setValue(dlg.taxonSet, prior2);
+                        int i = 1;
+                        String id = dlg.taxonSet.getID();
+                        while (doc.pluginmap.containsKey(dlg.taxonSet.getID()) && doc.pluginmap.get(dlg.taxonSet.getID()) != dlg.taxonSet) {
+                        	dlg.taxonSet.setID(id + i);
+                        	i++;
+                        }
+                        BEASTObjectPanel.addPluginToMap(dlg.taxonSet, doc);
+                        prior2.setID(dlg.taxonSet.getID() + ".prior");
+
+                    }
+                } catch (Exception e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                refreshPanel();
+            });
+
+
+        if (prior.distInput.getType() == null) {
+            try {
+                prior.distInput.setValue(new OneOnX(), prior);
+//                prior.distInput.setValue(null, prior);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+
+        }
+
+        List<BeautiSubTemplate> availableBEASTObjects = doc.getInputEditorFactory().getAvailableTemplates(prior.distInput, prior, null, doc);
+        JComboBox<BeautiSubTemplate> comboBox = new JComboBox<>(availableBEASTObjects.toArray(new BeautiSubTemplate[]{}));
+        comboBox.setName(text+".distr");
+
+        if (prior.distInput.get() != null) {
+            String id = prior.distInput.get().getID();
+            //id = BeautiDoc.parsePartition(id);
+            id = id.substring(0, id.indexOf('.'));
+            for (BeautiSubTemplate template : availableBEASTObjects) {
+                if (template.classInput.get() != null && template.shortClassName.equals(id)) {
+                    comboBox.setSelectedItem(template);
+                }
+            }
+        } else {
+            comboBox.setSelectedItem(BeautiConfig.NULL_TEMPLATE);
+        }
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                @SuppressWarnings("unchecked")
+				JComboBox<BeautiSubTemplate> comboBox = (JComboBox<BeautiSubTemplate>) e.getSource();
+                BeautiSubTemplate template = (BeautiSubTemplate) comboBox.getSelectedItem();
+                List<?> list = (List<?>) m_input.get();
+                BBDPrior prior = (BBDPrior) list.get(itemNr);
+
+//System.err.println("PRIOR" + beastObject2);
+//            	try {
+//					prior.m_distInput.setValue(beastObject2, prior);
+//				} catch (Exception e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+                try {
+                    //BEASTObject beastObject2 =
+                    template.createSubNet(new PartitionContext(""), prior, prior.distInput, true);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+                refreshPanel();
+            }
+        });
+        itemBox.add(comboBox);
+
+/*        JCheckBox isMonophyleticdBox = new JCheckBox(doc.beautiConfig.getInputLabel(prior, prior.isMonophyleticInput.getName()));
+        isMonophyleticdBox.setName(text+".isMonophyletic");
+        isMonophyleticdBox.setSelected(prior.isMonophyleticInput.get());
+        isMonophyleticdBox.setToolTipText(prior.isMonophyleticInput.getHTMLTipText());
+        isMonophyleticdBox.addActionListener(new MRCAPriorInputEditor.MRCAPriorActionListener(prior));
+        itemBox.add(isMonophyleticdBox);*/
+
         JButton deleteButton = new SmallButton("-", true);
-        deleteButton.setToolTipText("Delete this BBD Prior");
+        deleteButton.setToolTipText("Delete this calibration");
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Log.warning.println("Trying to delete a BBD Prior");
+                Log.warning.println("Trying to delete a calibration");
                 List<?> list = (List<?>) m_input.get();
                 BBDPrior prior = (BBDPrior) list.get(itemNr);
                 doc.disconnect(prior, "prior", "distribution");
+                doc.disconnect(prior, "tracelog", "log");
+                TipDatesRandomWalker operator = null;
+                TaxonSet taxonset = prior.taxonsetInput.get();
+                for (BEASTInterface o : taxonset.getOutputs()) {
+                    if (o instanceof TipDatesRandomWalker) {
+                        operator = (TipDatesRandomWalker) o;
+                    }
+                }
+                if (operator == null) {
+                    // should never happen
+                    return;
+                }
+                
+                // remove from list of operators
+                Object o = doc.mcmc.get().getInput("operator");
+                if (o instanceof Input<?>) {
+                    Input<List<Operator>> operatorInput = (Input<List<Operator>>) o;
+                    List<Operator> operators = operatorInput.get();
+                    operators.remove(operator);
+                }
+                
                 doc.unregisterPlugin(prior);
                 refreshPanel();
-            }        	
+            }
         });
-        add(deleteButton);
+        
+        itemBox.add(Box.createGlue());
+        itemBox.add(deleteButton);
+        add(itemBox);
+
+        // add operator
+        TipDatesRandomWalker operator = null;
+    	TaxonSet taxonset = prior.taxonsetInput.get();
+    	taxonset.initAndValidate();
+        
+    	for (BEASTInterface o : taxonset.getOutputs()) {
+            if (o instanceof TipDatesRandomWalker) {
+                operator = (TipDatesRandomWalker) o;
+            }
+        }
+    	
+    	if (operator == null) {
+            operator = new TipDatesRandomWalker();
+            operator.initByName("tree", prior.treeInput.get(), "taxonset", taxonset, "windowSize", 1.0, "weight", 1.0);
+    	}
+   	operator.setID("tipDatesSampler." + taxonset.getID());
+   	doc.mcmc.get().setInputValue("operator", operator);
     }
     
     Set<Taxon> getTaxonCandidates(BBDPrior prior) {
@@ -92,72 +254,5 @@ public class BBDPriorInputEditor extends BEASTObjectInputEditor implements Prior
             candidates.add(doc.getTaxon(taxon));
         }       
         return candidates;
-    }
-    
-    @Override
-    public List<Distribution> createDistribution(BeautiDoc doc) {
-        this.doc = doc;
-        BBDPrior prior = new BBDPrior();
-        try {
-            List<Tree> trees = new ArrayList<>();
-            getDoc().scrubAll(true, false);
-            State state = (State) doc.pluginmap.get("state");
-            for (StateNode node : state.stateNodeInput.get()) {
-                if (node instanceof Tree) { // && ((Tree) node).m_initial.get() != null) {
-                    trees.add((Tree) node);
-                }
-            }
-            int treeIndex = 0;
-            if (trees.size() > 1) {
-                String[] treeIDs = new String[trees.size()];
-                for (int j = 0; j < treeIDs.length; j++) {
-                    treeIDs[j] = trees.get(j).getID();
-                }
-                treeIndex = JOptionPane.showOptionDialog(null, "Select a tree", " selector",
-                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-                        treeIDs, trees.get(0));
-            }
-            if (treeIndex < 0) {
-                return null;
-            }
-            prior.treeInput.setValue(trees.get(treeIndex), prior);
-            TaxonSet taxonSet = new TaxonSet();
-            TaxonSetDialog dlg = new TaxonSetDialog(taxonSet, getTaxonCandidates(prior), doc);
-            if (!dlg.showDialog() || dlg.taxonSet.getID() == null || dlg.taxonSet.getID().trim().equals("")) {
-                return null;
-            }
-            taxonSet = dlg.taxonSet;
-            if (taxonSet.taxonsetInput.get().isEmpty()) {
-            	JOptionPane.showMessageDialog(doc.beauti, "At least one taxon should be included in the taxon set",
-            			"Error specifying taxon set", JOptionPane.ERROR_MESSAGE);
-            	return null;
-            }
-            int i = 1;
-            String id = taxonSet.getID();
-            while (doc.pluginmap.containsKey(taxonSet.getID()) && doc.pluginmap.get(taxonSet.getID()) != taxonSet) {
-            	taxonSet.setID(id + i);
-            	i++;
-            }
-            BEASTObjectPanel.addPluginToMap(taxonSet, doc);
-            prior.taxonsetInput.setValue(taxonSet, prior);
-            prior.setID(taxonSet.getID() + ".prior");
-            // this sets up the type
-            prior.distInput.setValue((ParametricDistribution)(new OneOnX()), prior);
-                        
-            Logger logger = (Logger) doc.pluginmap.get("tracelog");
-            logger.loggersInput.setValue(prior, logger);
-        } catch (Exception e) {
-            System.err.println(e);
-	}
-	List<Distribution> selectedPlugins = new ArrayList<>();
-	selectedPlugins.add(prior);
-        
-//	g_collapsedIDs.add(prior.getID());
-	return selectedPlugins;
-    }
-    
-    @Override
-    public String getDescription() {
-	return "BBD prior";
     }
 }
