1e4b2be36270727603e78788f4a400ea2d75a178
brj1 <brj1@sfu.ca>
2018-01-31 15:45:01 -0800

    BBPPrior inherits MRCAPrior

--- src/beast/math/distributions/BBDPrior.java
+++ src/beast/math/distributions/BBDPrior.java
@@ -27,165 +27,121 @@ package beast.math.distributions;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
 import beast.core.Description;
-import beast.core.Distribution;
-import beast.core.Input;
-import beast.core.State;
-import beast.evolution.alignment.TaxonSet;
-import beast.evolution.tree.Tree;
-import java.util.LinkedHashSet;
-import java.util.Set;
+
 
 /**
  * Blind dating prior.
  * @author Bradley R. Jones
  */
 @Description("Blind dating prior")
-public class BBDPrior extends Distribution {
-    public final Input<Tree> treeInput = new Input<>("tree", "the tree containing the taxon set", Input.Validate.REQUIRED);
-    public final Input<ParametricDistribution> distInput = new Input<>("distr",
-            "distribution used to calculate prior over MRCA time, "
-                    + "e.g. normal, beta, gamma.", Input.Validate.REQUIRED);
-    public final Input<TaxonSet> taxonsetInput = new Input<>("taxonset",
-            "set of taxa to date");
-    
+public class BBDPrior extends MRCAPrior {
     double[] oriDate;
-    ParametricDistribution dist;
-    Tree tree;
-    // number of taxa in taxon set
-    int nrOfTaxa = -1;
-    // array of flags to indicate which taxa are in the set
-    Set<String> isInTaxaSet = new LinkedHashSet<>();
 
-    // array of indices of taxa
-    int[] taxonIndex;
-    
-    boolean initialised = false;
-    
     @Override
     public void initAndValidate() {
-        dist = distInput.get();
-        tree = treeInput.get();
+        super.dist = super.distInput.get();
+        super.tree = super.treeInput.get();
         final List<String> taxaNames = new ArrayList<>();
-        for (final String taxon : tree.getTaxaNames()) {
+        for (final String taxon : super.tree.getTaxaNames()) {
             taxaNames.add(taxon);
         }
         // determine nr of taxa in taxon set
-        if (taxonsetInput.get() != null) {
-            List<String> set = taxonsetInput.get().asStringList();
-            nrOfTaxa = set.size();
+        if (super.taxonsetInput.get() != null) {
+            List<String> set = super.taxonsetInput.get().asStringList();
+            super.nrOfTaxa = set.size();
         } else {
             // assume all taxa
-            nrOfTaxa = taxaNames.size();
+            super.nrOfTaxa = taxaNames.size();
         }
-        initialised = false;
+       super.initialised = false;
     }
     
     protected void initialise() {
         List<String> set = null;
         int k = 0;
-        if (taxonsetInput.get() != null) {
-            set = taxonsetInput.get().asStringList();
+        if (super.taxonsetInput.get() != null) {
+            set = super.taxonsetInput.get().asStringList();
         }
         final List<String> taxaNames = new ArrayList<>();
-        for (final String taxon : tree.getTaxaNames()) {
+        for (final String taxon : super.tree.getTaxaNames()) {
             taxaNames.add(taxon);
         }
 
-        taxonIndex = new int[nrOfTaxa];
+        super.taxonIndex = new int[super.nrOfTaxa];
         if ( set != null )  {  // m_taxonset.get() != null) {
-            isInTaxaSet.clear();
+            super.isInTaxaSet.clear();
             for (final String taxon : set) {
                 final int taxonIndex_ = taxaNames.indexOf(taxon);
                 if (taxonIndex_ < 0) {
                     throw new RuntimeException("Cannot find taxon " + taxon + " in data");
                 }
-                if (isInTaxaSet.contains(taxon)) {
+                if (super.isInTaxaSet.contains(taxon)) {
                     throw new RuntimeException("Taxon " + taxon + " is defined multiple times, while they should be unique");
                 }
-                isInTaxaSet.add(taxon);
-                taxonIndex[k++] = taxonIndex_;
+                super.isInTaxaSet.add(taxon);
+                super.taxonIndex[k++] = taxonIndex_;
             }
         } else {
-            for (int i = 0; i < nrOfTaxa; i++) {
-                taxonIndex[i] = i;
+            for (int i = 0; i < super.nrOfTaxa; i++) {
+                super.taxonIndex[i] = i;
             }
         }
         
-        oriDate = new double[nrOfTaxa];
+        oriDate = new double[super.nrOfTaxa];
         k = 0;
-        for (final int i : taxonIndex) {
-            oriDate[k++] = tree.getNode(i).getDate();
+        for (final int i : super.taxonIndex) {
+            oriDate[k++] = super.tree.getNode(i).getDate();
         }
         
-        initialised = true;
+        super.initialised = true;
     }
     
     @Override
     public double calculateLogP() {
-    	if (!initialised) {
+    	if (!super.initialised) {
     		initialise();
     	}
-        logP = 0;
+        super.logP = 0;
         // tip date
-        if (dist == null) {
-            return logP;
+        if (super.dist == null) {
+            return super.logP;
         }
         int k = 0;
         for (final int i  : taxonIndex) {
-            logP += dist.logDensity(oriDate[k++] - tree.getNode(i).getDate());
+            super.logP += super.dist.logDensity(oriDate[k++] - super.tree.getNode(i).getDate());
         }
-        return logP;
+        return super.logP;
     }
     
-    @Override
-    public void store() {
-        super.store();
-    }
-
-    @Override
-    public void restore() {
-        super.restore();
-    }
-
-    @Override
-    protected boolean requiresRecalculation() {
-        return super.requiresRecalculation();
-    }
     
   /**
      * Loggable interface implementation follows *
      */
+    @Override
     public void init(final PrintStream out) {
-    	if (!initialised) {
+    	if (!super.initialised) {
     		initialise();
     	}
-        if (dist != null) {
+        if (super.dist != null) {
             out.print("logP(deltaTime(" + getID() + "))\t");
         }
-        for (final int i : taxonIndex) {
+        for (final int i : super.taxonIndex) {
             out.print("deltaTime(" + tree.getTaxaNames()[i] + ")\t");
         }
     }
     
     @Override
     public void log(final int sample, final PrintStream out) {
-        if (dist != null) {
+        if (super.dist != null) {
             out.print(getCurrentLogP() + "\t");
         }
         int k = 0;
-        for (final int i : taxonIndex) {
-            out.print(oriDate[k++] - tree.getNode(i).getDate() + "\t");
+        for (final int i : super.taxonIndex) {
+            out.print(oriDate[k++] - super.tree.getNode(i).getDate() + "\t");
         }
     }
     
-    @Override
-    public void close(final PrintStream out) {
-        // nothing to do
-    }
-
     /**
      * Valuable interface implementation follows, first dimension is log likelihood, second the time *
      */
@@ -221,19 +177,5 @@ public class BBDPrior extends Distribution {
             default:
                 return 0;
         }
-    }
-
-    @Override
-    public void sample(final State state, final Random random) {
-    }
-
-    @Override
-    public List<String> getArguments() {
-        return null;
-    }
-
-    @Override
-    public List<String> getConditions() {
-        return null;
     }
 }
