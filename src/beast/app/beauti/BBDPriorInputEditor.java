c716c209fc637c017ad09c5f7818a33dca444917
brj1 <brj1@sfu.ca>
2018-02-01 17:23:00 -0800

    fixed operator creation

--- src/beast/app/beauti/BBDPriorInputEditor.java
+++ src/beast/app/beauti/BBDPriorInputEditor.java
@@ -230,9 +230,9 @@ public class BBDPriorInputEditor extends InputEditor.Base {
     	if (operator == null) {
             operator = new TipDatesRandomWalker();
             operator.initByName("tree", prior.treeInput.get(), "taxonset", taxonset, "windowSize", 1.0, "weight", 1.0);
+            operator.setID("tipDatesSampler." + taxonset.getID());
+            doc.mcmc.get().setInputValue("operator", operator);
     	}
-   	operator.setID("tipDatesSampler." + taxonset.getID());
-   	doc.mcmc.get().setInputValue("operator", operator);
         
         prior.isMonophyleticInput.set(false);
         prior.onlyUseTipsInput.set(true);
