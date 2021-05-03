9473df5a1c9500ceb74131483ab63b0448ec7069
brad <brad@BRAD>
2021-05-03 12:59:51 -0700

    fixed recursive tip date bug

--- src/beast/evolution/operators/TipDatesScalerRecursive.java
+++ src/beast/evolution/operators/TipDatesScalerRecursive.java
@@ -88,7 +88,7 @@ public class TipDatesScalerRecursive extends TipDatesScalerPadded {
             // push close parent node down
             } else if (node.getHeight() > newValue && (parentHeight - node.getHeight()) <= padding) {
                 final double maxHeight = maxChildHeight(parent, node);
-                final double range = parentHeight - Math.min(maxHeight, newValue);
+                final double range = Math.min(parentHeight - maxHeight, newValue);
                 final double nextShift = Randomizer.nextDouble() * range;
 
                 if (nextShift > padding) {
